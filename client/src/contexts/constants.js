c6b96acc5d29aa0c499da7dbdaed9c597afbc131
namluu <luuhoainam97@gmail.com>
2021-08-24 22:28:03 +0700

    Prepare deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,4 +1,7 @@
-export const apiUrl = process.env.NODE_ENV !== "production" ? "http://localhost:5000/api" : "someDeployedURL";
+export const apiUrl =
+    process.env.NODE_ENV !== "production"
+        ? "http://localhost:5000/api"
+        : "https://shielded-falls-52409.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
 
